2662acf29eb23d3d3e13d1b6a31c93c435f6a8fe
Vaibhav negi <139096954+vaibhanNegi@users.noreply.github.com>
2023-10-23 22:39:58 +0530

    Add files via upload

---
+++ cal2.js
@@ -0,0 +1,29 @@
+const buttonsE1 = document.querySelectorAll("button");
+const inputFieldE1 = document.getElementById("result")
+for(let i = 0; i < buttonsE1.length; i++){
+  buttonsE1[i].addEventListener("click", ()=>{
+    const buttonValue = buttonsE1[i].textContent;
+    if(buttonValue === "C"){
+      clearResult()
+      
+    } else if(buttonValue === "="){
+      calculateResult()
+    } else{
+      appValue(buttonValue);
+    }
+  });
+}
+function clearResult(){
+  inputFieldE1.value ="";
+
+
+}
+function calculateResult(){
+  inputFieldE1.value = eval(inputFieldE1.value)
+
+
+}
+function appValue(buttonValue){
+  inputFieldE1.value +=  buttonValue;
+
+}
